fd2e282d625ee2a9c568ce3b35f37bebc25fa82a
Erjoh <erjik1902@gmail.com>
2023-07-20 13:23:31 +0600

    dsad

--- script.js
+++ script.js
@@ -1,16 +1,71 @@
-// const handleResponce = () => {
-//     fetch('https://swapi.dev/api/people/1/')
-//         .then(responce => responce.json())
-//         .then(data => {
-//             fetch("https://swapi.dev/api/vehicles/14/")
-//                 .then(responce => responce.json())
-//                 .then(vehicles => {
-//                     fetch("https://swapi.dev/api/starships/12/")
-//                         .then(responce => responce.json())
-//                         .then(starships => alert(`Hello ${data.name}, you are ${data.birth_year}, your ${vehicles.name}, your ${starships.name}`))
-//                 })
-//         })
-//         .catch(error => console.log(error))
+const handleResponce = () => {
+    fetch('https://swapi.dev/api/people/1/')
+        .then(responce => responce.json())
+        .then(data => alert(`hello ${data.name} and you ${data.birth_year} kg `))
+        .catch(error => console.log(error))
+}
+
+handleResponce()
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+// const checkSum = (array) => {
+//     let sum = 0;
+//     for (let index = 0; index < array.length; index++){
+//         sum += array[index]
+//     }
+//     return sum;
 // }
 //
-// handleResponce()
+// console.log(checkSum(array))
+
+// const newArray = 'lorem ipsum dolor sit amet'
+// const secondArr = newArray.split('').reduce((acc, el, idx) => {
+//     return [...acc, idx % 2 === 0 ? el : false]
+// }, [])
+//
+// console.log(secondArr)
+//
+// const array = 'lorem ipsum dolor sit amet'
+//
+// const checkStr = (array) => {
+//     let string = '';
+//     for (let index = 0; index < array.length; index++){
+//         string += array[index]
+//     }
+//     return string;
+// }
+//
+// console.log(checkStr(array))
+
+// const arr = [1, 2, 3, 4, 5]
+// const result = (arr) => {
+//     let sum = 0
+//     for (let index = 0; index < arr.length; index++){
+//         sum += arr[index]
+//     }
+//     return sum;
+// }
+//
+// console.log(result(arr))
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const result = (arr) => {
+//     let sum = 0
+//     for (let index = 0; index < arr.length; index++){
+//         sum += arr[index]
+//     }
+//     return sum;
+// }
+//
+// console.log(result(arr))
+
+
+const result = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === 4){
+            return 'Есть'
+        }
+    }
+}
+console.log(result([1, 2, 5, 9, 4, 13, 4, 10]))
